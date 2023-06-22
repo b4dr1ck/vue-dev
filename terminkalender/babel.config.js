6c15e4024eabe1e360d5d61e56e2098431941399
badrick <reiter.patrick@liwest.at>
2023-06-22 10:56:53 +0200

    NEW:  terminkalender/: Anlegen und Vorbereiten des Projekts: Terminkalender

---
+++ terminkalender/babel.config.js
@@ -0,0 +1,16 @@
+// module.exports = {
+//   presets: [
+//     '@vue/cli-plugin-babel/preset'
+//   ]
+// }
+
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        corejs: "core-js@3",
+      },
+    ],
+  ],
+};
---
+++ terminkalender/src/components/CalendarWeek.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="calender-week">
+    <div class="card-group">
+      <CalendarDay v-for="i in 7" :key="i" />
+    </div>
+  </div>
+</template>
+
+<script>
+import CalendarDay from "./CalendarDay.vue";
+export default {
+  name: "CalendarWeek",
+  components: {
+    CalendarDay,
+  },
+};
+</script>
+
+// scoped: das CSS gilt nur lokal für diese Component
+<style scoped></style>
