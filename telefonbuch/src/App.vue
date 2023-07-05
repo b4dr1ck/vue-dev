2260a557a5e00527aa84cd11cec622a5041ba78e
badrick <patrick.reiter@itsv.at>
2023-07-05 09:41:31 +0200

    NEW: telefonbuch: neues Test-Projekt erstellt (zum Ueben)

---
+++ telefonbuch/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <MySexyButtonVue buttonText="OK" />
+    <MySexyButtonVue buttonText="NOK" />
+    <MySexyButtonVue buttonText="FUK" />
+  </div>
+</template>
+
+<script>
+import MySexyButtonVue from "./components/MySexyButton.vue";
+
+export default {
+  name: "App",
+  components: {
+    MySexyButtonVue,
+  },
+  data() {
+    return {
+      msg: "Hello World",
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  font-family: "Arial";
+}
+
+h1 {
+  color: blueviolet;
+}
+</style>
---
+++ telefonbuch/src/components/MySexyButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <button @click="showAlert(buttonText)">{{ buttonText }}</button>
+    <br />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MySexyButton",
+  props: ["buttonText"],
+  methods: {
+    showAlert(text) {
+      alert("ButtonText ist: " + text);
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  background: pink;
+  border: none;
+  color: darkmagenta;
+  padding: 5px;
+  font-size: 14px;
+  cursor: pointer;
+  font-weight: bolder;
+  margin: 5px;
+  width: 50px;
+  box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.5);
+}
+button:hover {
+  background: rgb(255, 229, 234);
+}
+</style>
